a60552a386edc0640963365a2f0e0bc5c7ba39a1
Ethan Minns <110484771+Varcrow@users.noreply.github.com>
2025-10-06 16:15:53 -0400

    Todo list, ParseSingleArg function

--- include/parsing.h
+++ include/parsing.h
@@ -5,10 +5,12 @@
 
 namespace stfm::parsing {
 
+using Arg = std::string;
 using ArgPair = std::pair<std::string, std::string>;
 using Error = std::string;
 
 bool ParseInput(const std::string& input);
+std::expected<Arg, Error> ParseArg(std::istringstream& iss);
 std::expected<ArgPair, Error> ParseArgPair(std::istringstream& iss);
 
 } // namespace stfm::parsing
--- src/parsing.cpp
+++ src/parsing.cpp
@@ -7,13 +7,21 @@
 
 namespace stfm::parsing {
 
+std::expected<Arg, Error> ParseArg(std::istringstream& iss) {
+    Arg arg;
+    if (!(iss >> arg)) {
+        return std::unexpected<Error>("Missing argument");
+    }
+    return arg;
+}
+
 std::expected<ArgPair, Error> ParseArgPair(std::istringstream& iss) {
     std::string arg1, arg2;
     if (!(iss >> arg1)) {
-        return std::unexpected<Error>("Incomplete command");
+        return std::unexpected<Error>("Missing first arg");
     }
     if (!(iss >> arg2)) {
-        return std::unexpected<Error>("Incomplete command");
+        return std::unexpected<Error>("Missing second arg");
     }
     return std::make_pair(arg1, arg2);
 }
